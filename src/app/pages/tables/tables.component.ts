664ac822669fd6302cb651cc6b5dee83d3a74ded
DEv <you@example.com>
2020-02-05 23:45:57 -0500

    Dashboard task page

--- src/app/pages/tables/tables.component.ts
+++ src/app/pages/tables/tables.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+interface Job {
+  id: number;
+  name: string;
+  category: string;
+  team: string;
+  percentage: number;
+  class: string;
+  image: string;
+}
 
 @Component({
   selector: 'app-tables',
@@ -9,7 +18,18 @@ export class TablesComponent implements OnInit {
 
   constructor() { }
 
+ 
+  
+  private jobs: Job[] = [];
+
   ngOnInit() {
+    this.jobs.push({id:1, name: "Matching engine", category: "Giving Back", team: "BPM", percentage: 100, class: "progress-bar bg-success", image: "../assets/img/theme/sketch.jpg"});
+    this.jobs.push({id:2, name: "OCR", category: "Research", team: "BPM", percentage: 70, class: "progress-bar bg-primary", image: "../assets/img/theme/react.jpg"});
+    this.jobs.push({id:3, name: "Cloud Computing", category: "Giving Back", team: "BPM", percentage: 50, class: "progress-bar bg-warning", image: "../assets/img/theme/sketch.jpg"});
+  }
+
+  assetsTrackBy(index: number, job: Job) {
+    return job.id;
   }
 
 }
